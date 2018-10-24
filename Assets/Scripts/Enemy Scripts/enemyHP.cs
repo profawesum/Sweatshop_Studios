e81c2a62f7a4013b799429fdf2bf37d65bcaaeac
TheSandstorm <41430338+TheSandstorm@users.noreply.github.com>
2018-10-24 16:42:12 +1300

    Making levels
    Starting to make level one

--- Assets/Scripts/Enemy Scripts/enemyHP.cs
+++ Assets/Scripts/Enemy Scripts/enemyHP.cs
@@ -5,23 +5,40 @@ using UnityEngine;
 public class enemyHP : MonoBehaviour {
 
 
+    public float Hp = 2;
+
+
 	// Use this for initialization
 	void Start () {
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+      
+        
+        
+        //checks value of hp
+        if (Hp <= 0)
+        {
+            //destroys the enemy
+            Destroy(gameObject);
+        }
+
+
+
     }
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
+
+
+    void OnTriggerEnter2D(Collider2D other) {
 
 
         //checks to see if playerWeapon 
         if (other.tag == "PlayerWeapon")
-        {   //destroys the enemy
-            Destroy(gameObject);
-
+        {
+            //reduce Hp by 1
+            Hp -= 1;
         }
     }
 }
